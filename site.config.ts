63348d1c77b109b6806912bbbea4744d1cbe3595
FatsWang <57435874+FatsWang@users.noreply.github.com>
2022-04-24 13:26:25 +0800

    Update site.config.ts
    修改信息

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b1b0c72c9d494e118f1835b6ac2578e1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '常胜将军王胖胖',
+  domain: 'https://cnotion.notion.site/b1b0c72c9d494e118f1835b6ac2578e1',
+  author: '王胖胖',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
